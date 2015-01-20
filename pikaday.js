f1d1d9e65c6b207423f48d78ef91f008e357ec79
Ales Rippl <ales.rippl@gooddata.com>
2015-01-20 14:21:02 +0100

    reverting

--- pikaday.js
+++ pikaday.js
@@ -4,8 +4,30 @@
  * Copyright © 2014 David Bushell | BSD & MIT license | https://github.com/dbushell/Pikaday
  */
 
-define(
-function(moment)
+
+(function (root, factory)
+{
+    'use strict';
+
+    var moment;
+    if (typeof exports === 'object') {
+        // CommonJS module
+        // Load moment.js as an optional dependency
+        try { moment = require('moment'); } catch (e) {}
+        module.exports = factory(moment);
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(function (req)
+        {
+            // Load moment.js as an optional dependency
+            var id = 'moment';
+            try { moment = req(id); } catch (e) {}
+            return factory(moment);
+        });
+    } else {
+        root.Pikaday = factory(root.moment);
+    }
+}(this, function (moment)
 {
     'use strict';
 
@@ -991,5 +1013,4 @@ function(moment)
     };
 
     return Pikaday;
-
-});
+}));
